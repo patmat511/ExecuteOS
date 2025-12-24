cdcf24e5ca7867715616c34511d6483100457dc3
Patryk Matusiak <128796303+patmat511@users.noreply.github.com>
2025-12-24 20:35:25 +0100

    feat: Add ITaskRepository

--- ExecuteOS.Server/Modules/Tasks/Interfaces/ITaskRepository.cs
+++ ExecuteOS.Server/Modules/Tasks/Interfaces/ITaskRepository.cs
@@ -1,7 +1,17 @@
-﻿namespace ExecuteOS.Server.Modules.Tasks.Interfaces
+﻿using ExecuteOS.Server.Common.Enums;
+using ExecuteOS.Server.Modules.Tasks.Models;
+
+namespace ExecuteOS.Server.Modules.Tasks.Interfaces
 {
     public interface ITaskRepository
     {
-
+        Task<TaskItem> GetAllAsnync(CancellationToken cancellationToken = default);
+        Task<TaskItem?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+        Task<TaskItem> GetByUserIdAsync(Guid userId, CancellationToken cancellationToken = default);
+        Task<TaskItem> GetByStatusAsync(Status status, CancellationToken cancellationToken = default);
+        Task<TaskItem> AddAsync(TaskItem task, CancellationToken cancellationToken = default);
+        Task<TaskItem> UpdateAsync(TaskItem task, CancellationToken cancellationToken = default);
+        Task<TaskItem> DeleteAsync(Guid id, CancellationToken cancellationToken = default);
+        Task<bool> ExistsAsync(Guid id, CancellationToken cancellationToken = default);
     }
 }
