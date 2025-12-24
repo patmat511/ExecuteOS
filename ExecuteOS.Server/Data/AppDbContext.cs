2042b61f16f3ef5e2fd0fd4d8ca61591bb4cda30
Patryk Matusiak <128796303+patmat511@users.noreply.github.com>
2025-12-24 23:14:46 +0100

    feat: Add ModelBuilder for TaskItem DbSet

--- ExecuteOS.Server/Data/AppDbContext.cs
+++ ExecuteOS.Server/Data/AppDbContext.cs
@@ -14,6 +14,42 @@ namespace ExecuteOS.Server.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<TaskItem>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+
+                entity.Property(e => e.Title)
+                .IsRequired()
+                .HasMaxLength(200);
+
+                entity.Property(e => e.Description)
+                .HasMaxLength(2000);
+
+                entity.Property(e => e.Status)
+                .IsRequired()
+                .HasConversion<string>();
+
+                entity.Property(e => e.Priority)
+                .IsRequired()
+                .HasConversion<string>();
+
+                entity.Property(e => e.EstimatedHours)
+                .HasPrecision(10, 2);
+
+                entity.Property(e => e.ActualHours)
+                .HasPrecision(10, 2);
+
+                entity.Property(e => e.CreatedAt)
+                .IsRequired();
+
+                entity.Property(e => e.UpdatedAt)
+                .IsRequired();
+
+                entity.HasIndex(e => e.Status);
+                entity.HasIndex(e => e.UserId);
+                entity.HasIndex(e => e.CreatedAt);
+            });
         }
     }
 }
